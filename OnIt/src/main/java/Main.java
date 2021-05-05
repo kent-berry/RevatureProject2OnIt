b679135db017ea856ea144c3a50d165a7b5a845f
hassanshallal <hshallal@icloud.com>
2021-05-05 09:20:14 -0600

    starter_for_ec2

--- OnIt/src/main/java/Main.java
+++ OnIt/src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.sql.SQLException;
 
 import connection.ConnectionUtil;
 
+
+// This main is for testing purposes, this is a WAR and no main class is needed at the end
 public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
