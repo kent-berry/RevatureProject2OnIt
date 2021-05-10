212b73a54761d533fabcc4e4556392850f097ec2
Jerdmann94 <62105705+Jerdmann94@users.noreply.github.com>
2021-05-10 17:10:04 -0500

    test for jenkins

--- OnIt/src/main/java/com/revature/controller/TaskController.java
+++ OnIt/src/main/java/com/revature/controller/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -33,6 +34,7 @@ import com.revature.service.TaskService;
 import com.revature.service.UserService;
 
 @Controller
+@CrossOrigin(origins = {"http://localhost:4200"}, allowCredentials = "true")
 public class TaskController {
 
 	@Autowired
--- OnIt/src/main/java/com/revature/controller/UserController.java
+++ OnIt/src/main/java/com/revature/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -36,6 +37,8 @@ import com.revature.service.UserService;
 @Controller
 //@RequestMapping(value = "/user")
 //@SessionAttributes("user")
+@CrossOrigin(origins = {"http://localhost:4200"}, allowCredentials = "true")
+
 public class UserController  {
 
 	@Autowired
