b0bf864f49d3effe9a969e2acb5de0e4c8abd33c
Ben30000 <47725693+Ben30000@users.noreply.github.com>
2021-05-15 08:47:43 -0500

    ok

--- OnIt/src/main/java/com/revature/controller/UserController.java
+++ OnIt/src/main/java/com/revature/controller/UserController.java
@@ -12,14 +12,15 @@ import javax.crypto.spec.PBEKeySpec;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.dto.DtoInteger;
@@ -56,17 +57,19 @@ public class UserController  {
 	private IUserService userservice = new UserService();
 	
 	
-	@GetMapping(value = "/checkActiveSession")
-	//@RequestMapping(method=RequestMethod.GET, value="/checkActiveSession/{sessionToken}")
+	//@GetMapping(value = "/checkActiveSession")
+	@RequestMapping(method=RequestMethod.GET, value="/checkActiveSession/{sessionToken}")
+	@ResponseStatus(code=HttpStatus.OK)
 	public  @ResponseBody User checkActiveSession() {
 		
 		//User authorizedUser = userservice.getUserFromSessionToken(sessionToken);
 		User authorizedUser = new User();
-		if(authorizedUser != null) {
+		return authorizedUser;
+		/*if(authorizedUser != null) {
 			return authorizedUser;
 		} else {
 			return null;
-		}
+		}*/
 	}
 	
 	@PostMapping(value = "/register")
