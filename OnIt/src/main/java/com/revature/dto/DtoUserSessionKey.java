6bf8cb7591e4c29df234b114aa475708ca5efe22
Ben30000 <47725693+Ben30000@users.noreply.github.com>
2021-05-15 13:31:55 -0500

    ok

--- OnIt/src/main/java/com/revature/dto/DtoUserSessionKey.java
+++ OnIt/src/main/java/com/revature/dto/DtoUserSessionKey.java
@@ -1,21 +1,18 @@
 package com.revature.dto;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class DtoUserSessionKey {
 
 	private String id;
 	private String email;
 	private String sessionToken;
 	
-	
-	
-	public DtoUserSessionKey(String id, String email, String sessionToken) {
+	public DtoUserSessionKey() {
 		super();
-		this.id = id;
-		this.email = email;
-		this.sessionToken = sessionToken;
+		// 
 	}
-	
-	
 	
 	public String getId() {
 		return id;
